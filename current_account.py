5d78b91929d1b8c3accf99903fe3ef4c6df45563
IzzyCoding17 <izzysenpeak17@gmal.com>
2025-06-13 22:27:27 +0100

    update on Deposit Function

--- current_account.py
+++ current_account.py
@@ -1,5 +1,11 @@
 from account import Account
 
 class CurrentAccount(Account):
-    def __init__(self, balance):
+    def __init__(self, balance,):
         Account.__init__(self, balance)
+
+    def deposit(self, amount):
+
+       if amount >= 0:
+           self.balance += amount
+
--- pages/current_account_page.py
+++ pages/current_account_page.py
@@ -1,10 +1,22 @@
 import streamlit as st
+from current_account import CurrentAccount
 
+if 'account' not in st.session_state:
+    st.session_state.account = CurrentAccount(balance= 10000 )
 st.set_page_config(page_title="Current Account", layout="centered")
-st.title("My Current Account")
-st.subheader("Balance: N10000")
-
+st.title("Current Account")
+st.subheader(f"Balance: N{st.session_state.account.balance}")
 with st.form("current_account_form"):
-    amount = st.number_input("Enter amount", min_value=1000)
+    amount = st.number_input("Enter amount", min_value=1000, step=100)
     operations = st.selectbox("Deposit or withdraw", ("Deposit", "Withdraw"))
     submit = st.form_submit_button("Submit")
+
+    if submit:
+        try:
+            if operations == "Deposit":
+                st.session_state.account.deposit(amount)
+                st.success(f"Successfuly Depositted N{amount}")
+            # add elif statement here for withdraw function
+
+        except ValueError as e:
+            st.error(str(e))
