483ea49ede62bb5d51d5dad1983b4fab27bd7eb7
Tuno-create <benbweseh@gmail.com>
2025-06-17 16:23:58 +0100

    updated error and success messages

--- pages/current_account_page.py
+++ pages/current_account_page.py
@@ -2,7 +2,7 @@ import streamlit as st
 from current_account import CurrentAccount
 
 if 'account' not in st.session_state:
-    st.session_state.account = CurrentAccount(balance= 10000 )
+    st.session_state.account = CurrentAccount(balance=10000)
 if 'msg' not in st.session_state:
     st.session_state.msg = ""
 if 'msg_type' not in st.session_state:
@@ -11,15 +11,17 @@ if 'msg_type' not in st.session_state:
 st.set_page_config(page_title="Current Account", layout="centered")
 st.title("Current Account")
 
-# Show message at the top of the page
+# Show message at the top
 if st.session_state.msg:
     if st.session_state.msg_type == "success":
         st.success(st.session_state.msg)
     elif st.session_state.msg_type == "error":
         st.error(st.session_state.msg)
+    st.session_state.msg = ""
+    st.session_state.msg_type = ""
 
 balance_placeholder = st.empty()
-balance_placeholder.subheader(f"Balance: ${st.session_state.account.balance}")
+balance_placeholder.subheader(f"Balance: ₦{st.session_state.account.balance}")
 
 with st.form("current_account_form"):
     amount = st.number_input("Enter amount", min_value=1000, step=100)
@@ -32,7 +34,6 @@ with st.form("current_account_form"):
                 st.session_state.account.deposit(amount)
                 st.session_state.msg = f"✅Deposit of ₦{amount} successful!"
                 st.session_state.msg_type = "success"
-            # add elif statement here for withdraw function
             elif operations == "Withdraw":
                 if amount > st.session_state.account.balance:
                     st.session_state.msg = f"❌Error: Insufficient Funds"
@@ -41,7 +42,8 @@ with st.form("current_account_form"):
                     st.session_state.account.withdraw(amount)
                     st.session_state.msg = f"✅Withdrawal of ₦{amount} successful!"
                     st.session_state.msg_type = "success"
-            balance_placeholder.subheader(f"Balance: ₦{st.session_state.account.balance}")
+            st.rerun()
         except ValueError as e:
-            st.error(f"An error occurred: {e}")
+            st.session_state.msg = f"An error occurred: {e}"
             st.session_state.msg_type = "error"
+            st.rerun
